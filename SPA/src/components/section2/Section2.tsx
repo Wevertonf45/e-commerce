e26a23120abafd81fb71ee2f5f3389e9baa5b22c
Weverton Santos <weverton07102005@gmail.com>
2025-12-31 15:58:21 -0300

    fix: remove pasta data

--- SPA/src/components/section2/Section2.tsx
+++ SPA/src/components/section2/Section2.tsx
@@ -1,26 +1,25 @@
-import { images } from "@/data/ImagesData"
-import Section from "../ui/section/Section"
-import Image from "next/image"
-import './Section2.css';
+import Section from "../ui/section/Section";
+import "./Section2.css";
 
 function Section2() {
     return (
         <Section id="section2">
-            <div className="images-container">
-                {images.map((item, index) => (
-                    <div className="image-wrapper" key={index}>
-                        <Image
-                            src={item.src}
-                            alt={item.alt}
-                            width={item.width}
-                            height={item.height}
-                        />
-                        <span className="overlay-text">Ver coleção</span>
-                    </div>
-                ))}
+            <div className="image-card image1">
+                <p>Casacos</p>
+                <span>Ver coleção →</span>
+            </div>
+
+            <div className="image-card image2">
+                <p>Vestidos</p>
+                <span>Ver coleção →</span>
+            </div>
+
+            <div className="image-card image3">
+                <p>Blazers</p>
+                <span>Ver coleção →</span>
             </div>
         </Section>
-    )
+    );
 }
 
-export default Section2
+export default Section2;
